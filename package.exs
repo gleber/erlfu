9b0c97b451b2ecae2b599b61ecfab35e08f661ef
Gleb Peregud <gleber.p@gmail.com>
2012-11-22 11:10:49 +0100

    release v0.9.0 (added expm manifest)

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "erlfu", description: "Futures/promises for Erlang implemented as garbage collected processes",
+                 version: "0.9.0", keywords: ["future", "promise", "language extension", "gc"], 
+                 maintainers: [[name: "Gleb Peregud", 
+                                email: "gleber.p@gmail.com"]],
+                 repositories: [[github: "gleber/erlfu"]])
+    
